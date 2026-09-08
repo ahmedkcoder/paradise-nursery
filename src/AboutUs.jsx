d0e517054bf85fb418703b8b8e6b689cb20b26b4
Ahmed Mehmood <ahmedkahmedk259@gmail.com>
2026-09-08 05:23:51 +0500

    Add AboutUs component with description

---
+++ src/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <p className="about-us-description">
+        Welcome to Paradise Nursery, where we bring nature closer to your home. 
+        Our mission is to provide a wide variety of healthy, vibrant houseplants 
+        that purify the air and enhance your living spaces. Whether you are a 
+        seasoned plant enthusiast or just starting your green journey, we are here 
+        to help you cultivate your indoor oasis.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
